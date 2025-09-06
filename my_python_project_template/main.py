b64569ab78731cf7afa7ec6e451a06d78050517b
sthitapati <sthita.pati@gmail.com>
2025-09-06 18:34:31 +0100

    edited main function

---
+++ my_python_project_template/main.py
@@ -0,0 +1,13 @@
+# You can write your main function here and then write subsequent files/ modules in a heirarchical/ modular manner.
+
+def main():
+    print("Hello, this is my customizable Python project template!")
+    # You can add more functionality here as needed.
+    # For example, you can call other functions or classes defined in other modules.
+    # Example:
+    # from my_python_project_template.module import some_function
+    # some_function()
+    pass
+
+if __name__ == "__main__":
+    main()
